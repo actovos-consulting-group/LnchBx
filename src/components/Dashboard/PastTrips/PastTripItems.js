1c6efba878552b890c66eba29d3435c70154c77c
Daniel France <dan@ittybam.com>
2020-05-19 21:13:14 -0500

    removed unused components

--- src/components/Dashboard/PastTrips/PastTripItems.js
+++ src/components/Dashboard/PastTrips/PastTripItems.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import styled, {div, css} from 'styled-components';
+import styled from 'styled-components';
 import MyTheme from '../../../theme';
 import PastTripItem from './PastTripItem/PastTripItem';
 
 const PastTripContainer = styled.div`
-border: 1px solid ${MyTheme.colors.black};
-width: 100%;
-`
+  border: 1px solid ${MyTheme.colors.black};
+  width: 100%;
+`;
 
 const PastTripItems = () => (
-    <PastTripContainer>
-        <PastTripItem />
-        <PastTripItem />
-        <PastTripItem />
-        <PastTripItem />
-    </PastTripContainer>
+  <PastTripContainer>
+    <PastTripItem />
+    <PastTripItem />
+    <PastTripItem />
+    <PastTripItem />
+  </PastTripContainer>
 );
 
 export default PastTripItems;
